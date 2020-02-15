91e2a1b4a4acbc8ff2e1d01c274f0b4f8d8f1acf
Matt Wardle <mwardle.22@gmail.com>
2020-02-14 19:47:36 -0500

    Implement count-if function: returns # of items in collection that pass a predicate

---
+++ exercises/src/exercises/count-if.clj
@@ -0,0 +1,15 @@
+(ns exercises.check-triangle
+  (:require [clojure.test :refer [is]]))
+
+(defn count-if
+  [cond? values]
+  (count (filter cond? values)))
+
+(is (= 3 (count-if even? [1 2 3 4 5 6])))
+
+(is (= 1 (count-if keyword? ["foo" :bar 'baz])))
+
+(is (= 2
+       (count-if (fn [x]
+                   (= x (reverse x)))
+                 [[1 2 1] [1 2 3 4] [1]])))
