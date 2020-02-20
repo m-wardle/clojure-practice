b6b6be5b3efb837f265170ee0de418bb08be9b18
Matt Wardle <mwardle.22@gmail.com>
2020-02-19 19:47:23 -0500

    Implement function that changes a value in a collection, returning the original value and new collection

---
+++ exercises/src/exercises/get-and-set.clj
@@ -0,0 +1,25 @@
+(ns exercises.check-triangle
+  (:require [clojure.test :refer [is]]))
+
+(defn get-and-set
+  [key value collection]
+  [(get collection key)
+   (assoc collection key value)])
+
+(def x [1 2 3])
+
+(def y {:x "foo" :y "baz"})
+
+(get-and-set 0 2 x)
+
+(get-and-set :y "bar" y)
+
+(is (= [1 [2 3 4]] (get-and-set 0 2 [1 3 4])))
+
+(is (= ["foo"
+        {:a "baz"
+         :b "quux"}]
+       (get-and-set :a
+                    "baz"
+                    {:a "foo"
+                     :b "quux"})))
